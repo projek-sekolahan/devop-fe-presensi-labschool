d40419bbdea9c76b40d391f3dafc4ccfc1967a69
Ijoelel <fianarnatha13@gmail.com>
2024-02-23 18:38:02 +0700

    24-02-2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Suspense, lazy } from "react";
 import {
 	BrowserRouter as Router,
 	Routes,
@@ -7,23 +8,42 @@ import {
 import "./App.css";
 import { useEffect, useState } from "react";
 import Register from "./Pages/Register";
-import OtpInput from "./Pages/OtpInput";
-import Login from "./Pages/Login";
-import ChangePassword from "./Pages/ChangePassword";
-import Home from "./Pages/Home";
-import Profile from "./Pages/Profile";
-import Presensi from "./Pages/Presensi";
-import Riwayat from "./Pages/Riwayat";
-import Bantuan from "./Pages/Bantuan";
-import Notification from "./Pages/Notification";
-import Sakit from "./Pages/Sakit";
-import Ijin from "./Pages/Ijin";
-import Pengaturan from "./Pages/Pengaturan";
-import PresensiStaff from "./Pages/PresensiStaff";
-import FaceVerification from "./Pages/FaceVerification";
-import FaceCam from "./Pages/FaceCam";
-import Errors from "./Pages/Error";
-import RegisterFace from "./Pages/RegisterFace";
+// import OtpInput from "./Pages/OtpInput";
+import Loading from "./Pages/Loading";
+// import Login from "./Pages/Login";
+// import ChangePassword from "./Pages/ChangePassword";
+// import Home from "./Pages/Home";
+// import Profile from "./Pages/Profile";
+// import Presensi from "./Pages/Presensi";
+// import Riwayat from "./Pages/Riwayat";
+// import Bantuan from "./Pages/Bantuan";
+// import Notification from "./Pages/Notification";
+// import Sakit from "./Pages/Sakit";
+// import Ijin from "./Pages/Ijin";
+// import Pengaturan from "./Pages/Pengaturan";
+// import PresensiStaff from "./Pages/PresensiStaff";
+// import FaceVerification from "./Pages/FaceVerification";
+// import FaceCam from "./Pages/FaceCam";
+// import Errors from "./Pages/Error";
+// import RegisterFace from "./Pages/RegisterFace";
+
+const OtpInput = lazy(() => import("./Pages/OtpInput"));
+const Login = lazy(() => import("./Pages/Login"));
+const ChangePassword = lazy(() => import("./Pages/ChangePassword"));
+const Home = lazy(() => import("./Pages/Home"));
+const Profile = lazy(() => import("./Pages/Profile"));
+const Presensi = lazy(() => import("./Pages/Presensi"));
+const Riwayat = lazy(() => import("./Pages/Riwayat"));
+const Bantuan = lazy(() => import("./Pages/Bantuan"));
+const Notification = lazy(() => import("./Pages/Notification"));
+const Sakit = lazy(() => import("./Pages/Sakit"));
+const Ijin = lazy(() => import("./Pages/Ijin"));
+const Pengaturan = lazy(() => import("./Pages/Pengaturan"));
+const PresensiStaff = lazy(() => import("./Pages/PresensiStaff"));
+const FaceVerification = lazy(() => import("./Pages/FaceVerification"));
+const FaceCam = lazy(() => import("./Pages/FaceCam"));
+const Errors = lazy(() => import("./Pages/Error"));
+const RegisterFace = lazy(() => import("./Pages/RegisterFace"));
 
 function App() {
 	const [width, setWidth] = useState(window.screen.width);
@@ -34,7 +54,7 @@ function App() {
 	}, []);
 	return (
 		<Router>
-			{/* {width && width > 500 ? (
+			{width && width > 500 ? (
 				<div className="font-primary w-screen h-screen absolute left-0 top-0 z-50 flex justify-center items-center before:size-full before:bg-black before:opacity-40 backdrop-blur-sm before:absolute">
 					<div className="modal-box">
 						<h3 className="font-bold text-xl">Warning!</h3>
@@ -43,28 +63,33 @@ function App() {
 						</p>
 					</div>
 				</div>
-			) : null} */}
-
-			<Routes>
-				<Route path="/" Component={Register} />
-				<Route path="/verification" Component={OtpInput} />
-				<Route path="/facereg" Component={RegisterFace} />
-				<Route path="/login" Component={Login} />
-				<Route path="/password/reset" Component={ChangePassword} />
-				<Route path="/home" Component={Home} />
-				<Route path="/profile" Component={Profile} />
-				<Route path="/presensi" Component={Presensi} />
-				<Route path="/riwayat" Component={Riwayat} />
-				<Route path="/bantuan" Component={Bantuan} />
-				<Route path="/notifikasi" Component={Notification} />
-				<Route path="/presensi/staff" Component={PresensiStaff} />
-				<Route path="/presensi/verif" Component={FaceVerification} />
-				<Route path="/presensi/bukti" Component={Sakit} />
-				<Route path="/presensi/keterangan" Component={Ijin} />
-				<Route path="/setting" Component={Pengaturan} />
-				<Route path="/facecam" Component={FaceCam} />
-				<Route path="*" Component={Errors} />
-			</Routes>
+			) : null}
+			<Suspense fallback={<Loading />}>
+				<Routes>
+					<Route path="/" Component={Register} />
+					{/* <Route path="/loading" Component={Loading} /> */}
+					<Route path="/verification" Component={OtpInput} />
+					<Route path="/facereg" Component={RegisterFace} />
+					<Route path="/login" Component={Login} />
+					<Route path="/password/reset" Component={ChangePassword} />
+					<Route path="/home" Component={Home} />
+					<Route path="/profile" Component={Profile} />
+					<Route path="/presensi" Component={Presensi} />
+					<Route path="/riwayat" Component={Riwayat} />
+					<Route path="/bantuan" Component={Bantuan} />
+					<Route path="/notifikasi" Component={Notification} />
+					<Route path="/presensi/staff" Component={PresensiStaff} />
+					<Route
+						path="/presensi/verif"
+						Component={FaceVerification}
+					/>
+					<Route path="/presensi/bukti" Component={Sakit} />
+					<Route path="/presensi/keterangan" Component={Ijin} />
+					<Route path="/setting" Component={Pengaturan} />
+					<Route path="/facecam" Component={FaceCam} />
+					<Route path="*" Component={Errors} />
+				</Routes>
+			</Suspense>
 		</Router>
 	);
 }
