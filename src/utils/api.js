f225a4ee8456e4b90c349b8e9b75e6b4be5c1ce4
bagus farouk <b46usf@gmail.com>
2024-03-07 19:14:17 +0700

    commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,7 +9,7 @@ export const getCsrf = async () => {
 			withCredentials: true,
 		})
 		.then((response) => {
-			console.log('Response Headers:', response.headers);
+			console.log('Response Headers:', response.headers.get());
 			const cookieFromResponse = response.headers.get('Set-Cookie');
 			console.log('Cookie dari respons header:', cookieFromResponse);
 			// Jika cookie ditemukan, Anda juga bisa menetapkannya menggunakan js-cookies
