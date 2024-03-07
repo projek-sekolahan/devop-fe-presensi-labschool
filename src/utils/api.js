8cabbcfcd17d000980b44250bc47bd414d2fcd46
bagus farouk <b46usf@gmail.com>
2024-03-07 18:43:22 +0700

    commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,28 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
-const api_url = import.meta.env.VITE_API_URL;
+const api_url = "https://devop-sso.smalabschoolunesa1.sch.id";
 
 export const getCsrf = async () => {
 	const csrf = await axios
 		.get(`${api_url}/view/tokenGetCsrf`, {
 			withCredentials: true,
 		})
-		.then((response) => response);
+		.then((response) => {
+			const cookieFromResponse = response.headers.get('Set-Cookie');
+			console.log('Cookie dari respons header:', cookieFromResponse);
+			// Jika cookie ditemukan, Anda juga bisa menetapkannya menggunakan js-cookies
+			if (cookieFromResponse) {
+				const cookieData = cookieFromResponse.split(';')[0]; // Ambil bagian pertama dari cookie
+				const cookieNameValue = cookieData.split('='); // Pisahkan nama dan nilai cookie
+				const cookieName = cookieNameValue[0];
+				const cookieValue = cookieNameValue[1];
+				Cookies.set(cookieName, cookieValue);
+			}
+		}
+		);
 	// console.log(csrf);
-	return csrf;
+	console.log(csrf);
 };
 
 export const toLogin = async (key, formData) => {
