9bc758bbcc60f366a409d1517383c89fac24bc21
Ijoelel <fianarnatha13@gmail.com>
2024-03-16 01:03:11 +0700

    16-03-2024

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -34,61 +34,19 @@ export default function Register() {
 		register(getFormData(keys, values), (res) => {
 			if (res.status != 200 || res.data.info == "error") {
 				setStatus(false);
+				setResponse(res.data);
 			} else {
 				setStatus(true);
+				setResponse(res.data.data);
 			}
-			setResponse(res.data);
 		});
 		response
-			? console.log("change10", status, "and", response)
+			? console.log("change20", status, "and", response)
 			: "data akan segera masuk";
 		document.getElementById("my_modal_3").showModal();
 	};
 	return (
 		<div className="bg-primary-low font-primary text-white flex flex-col h-screen w-screen sm:w-[400px] sm:ml-[calc(50vw-200px)] pt-16 relative">
-			status ? (
-			<dialog id="my_modal_3" className="modal text-gray-600">
-				<div className="modal-box p-0 rounded-md">
-					<div className="relative flex items-center justify-between px-2 py-2 font-bold text-white bg-green-500 rounded-t">
-						<div className="relative flex items-center">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								strokeWidth="1.5"
-								stroke="currentColor"
-								className="inline w-6 h-6 mr-2 opacity-75"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-								/>
-							</svg>
-							<span>{data ? data.data.title : null}</span>
-						</div>
-						<form className="relative" method="dialog">
-							<button>
-								<svg
-									className="w-5 h-5 text-green-300 fill-current hover:text-white"
-									role="button"
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 20 20"
-								>
-									<title>Close</title>
-									<path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-								</svg>
-							</button>
-						</form>
-					</div>
-					<div className="p-3 bg-white border border-gray-300 rounded-b shadow-lg">
-						<span className="block">
-							{data ? data.data.message : null}
-						</span>
-					</div>
-				</div>
-			</dialog>
-			) : (
 			<dialog id="my_modal_3" className="modal text-gray-600">
 				<div className="modal-box p-0 rounded-md">
 					<div className="relative flex items-center justify-between px-2 py-2 font-bold text-white bg-red-500 rounded-t">
@@ -108,7 +66,7 @@ export default function Register() {
 								/>
 							</svg>
 
-							<span>{data ? data.title : null}</span>
+							<span>data.title</span>
 						</div>
 						<form className="relative" method="dialog">
 							<button>
@@ -126,14 +84,13 @@ export default function Register() {
 					</div>
 					<div className="p-3 bg-white border border-gray-300 rounded-b shadow-lg">
 						<span className="block">
-							Register Error, Harap periksa apakah data yang anda
-							masukkan benar, pastikan tidak menggunakan email
-							yang sudah terdaftar.
+							{status
+								? data.message
+								: "Register Error, Harap periksa apakah data yang anda masukkan benar, pastikan tidak menggunakan email yang sudah terdaftar."}
 						</span>
 					</div>
 				</div>
 			</dialog>
-			);
 			<h1 className="text-center text-4xl font-bold text-white ">
 				Register
 			</h1>
