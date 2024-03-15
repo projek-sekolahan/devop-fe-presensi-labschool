6db515d804810b64f2a76511a3dbd37cf198c752
Ijoelel <fianarnatha13@gmail.com>
2024-03-16 00:33:58 +0700

    16-03-2024

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -40,7 +40,7 @@ export default function Register() {
 			}
 			setResponse(res.data);
 		});
-		console.log("change", status, "and", response);
+		console.log("change1", status, "and", response);
 		document.getElementById("my_modal_3").showModal();
 	};
 	return (
