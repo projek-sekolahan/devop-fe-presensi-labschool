e03e5ced3a8ee3ad9e7681da80a9f8de58058f04
Ijoelel <fianarnatha13@gmail.com>
2024-03-16 10:20:58 +0700

    16-03-2024

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -4,7 +4,7 @@ import { register } from "../utils/api.js";
 import { getFormData } from "../utils/utils.js";
 import { useRef, useState } from "react";
 import Cookies from "js-cookie";
-import ModalNotification from "/src/Components/ModalNotification";
+import Swal from "sweetalert2";
 
 export default function Register() {
 	console.log("nice change");
@@ -36,65 +36,20 @@ export default function Register() {
 		];
 		register(getFormData(keys, values), (res) => {
 			if (res.status != 200 || res.data.info == "error") {
-				// setStatus("false");
 				setResponse(res.data);
-				titleRef.current.innerText = res.data.title;
-				messageRef.current.innerText =
-					"Register Error, Harap periksa apakah data yang anda masukkan benar, pastikan tidak menggunakan email yang sudah terdaftar.";
-				document.getElementById("my_modal_3").showModal();
-				redirect(`/${res.data.location}`);
 			} else {
-				// setStatus("true");
 				setResponse(res.data.data);
-				titleRef.current.innerText = res.data.data.title;
-				messageRef.current.innerHTML = res.data.data.message;
-				document.getElementById("my_modal_3").showModal();
-				redirect(`/${res.data.data.location}`);
 			}
 		});
 	};
+	Swal.fire({
+		title: 'Error!',
+		text: 'Do you want to continue',
+		icon: 'error',
+		confirmButtonText: 'Cool'
+	  })
 	return (
 		<div className="bg-primary-low font-primary text-white flex flex-col h-screen w-screen sm:w-[400px] sm:ml-[calc(50vw-200px)] pt-16 relative">
-			<dialog id="my_modal_3" className="modal text-gray-600">
-				<div className="modal-box p-0 rounded-md">
-					<div className="relative flex items-center justify-between px-2 py-2 font-bold text-white bg-red-500 rounded-t">
-						<div className="relative flex items-center">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								className="w-6 h-6"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-								/>
-							</svg>
-
-							<span ref={titleRef}></span>
-						</div>
-						<form className="relative" method="dialog">
-							<button>
-								<svg
-									className="w-5 h-5 text-green-300 fill-current hover:text-white"
-									role="button"
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 20 20"
-								>
-									<title>Close</title>
-									<path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-								</svg>
-							</button>
-						</form>
-					</div>
-					<div className="p-3 bg-white border border-gray-300 rounded-b shadow-lg">
-						<span className="block" ref={messageRef}></span>
-					</div>
-				</div>
-			</dialog>
 			<h1 className="text-center text-4xl font-bold text-white ">
 				Register
 			</h1>
