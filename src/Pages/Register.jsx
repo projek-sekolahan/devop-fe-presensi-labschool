ae58e7240917f6b1a84088c471ac797210fac8c1
Ijoelel <fianarnatha13@gmail.com>
2024-03-16 11:15:00 +0700

    16-03-2024

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -34,7 +34,7 @@ export default function Register() {
 			if (res.status != 200 || res.data.info == "error") {
 				Swal.fire({
 					titleText: res.data.title,
-					text: "change2 Harap periksa apakah nomor dan email belum digunakan!",
+					text: "change3 Harap periksa apakah nomor dan email belum digunakan!",
 					icon: "error",
 					allowOutsideClick: false,
 					allowEnterKey: false,
