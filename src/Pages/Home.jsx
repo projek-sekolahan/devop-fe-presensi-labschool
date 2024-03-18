cb09211a66a80e87be044e17731c78ca1984558f
Ijoelel <fianarnatha13@gmail.com>
2024-03-18 07:06:07 +0700

    18-03-2024

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -34,7 +34,7 @@ export default function Home() {
 		localStorage.getItem("AUTH_KEY"),
 		getFormData(key, values),
 		(res) => {
-			console.log(res.data.data);
+			console.log(parseJwt(res.data.data));
 		}
 	);
 
