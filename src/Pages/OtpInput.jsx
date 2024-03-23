27f80c5fe12d4526667adf1cee2e2fc8590ef094
Ijoelel <fianarnatha13@gmail.com>
2024-03-23 17:41:08 +0700

    23-03-2024

--- src/Pages/OtpInput.jsx
+++ src/Pages/OtpInput.jsx
@@ -94,7 +94,7 @@ export default function OtpInput() {
 			e.preventDefault();
 			console.log(res);
 
-			if (res.status == 200 && res.data) {
+			if (res.status == 200 && res.data.success == "Success") {
 				Swal.fire({
 					titleText: res.data.title,
 					text: "Kode OTP berhasil dikirim",
