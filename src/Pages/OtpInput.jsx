593ac36bb0d8272b411bb9f1c070ead0cb8d24d8
Ijoelel <fianarnatha13@gmail.com>
2024-03-29 02:27:54 +0700

    29-03-2024

--- src/Pages/OtpInput.jsx
+++ src/Pages/OtpInput.jsx
@@ -26,11 +26,12 @@ export default function OtpInput() {
 		formData.append("csrf_token", csrf);
 
 		verify(formData, (res) => {
+			console.log(res);
 			if (res.status == 200 && res.data.data) {
 				localStorage.setItem("regist_token", res.data.data.token);
 				Swal.fire({
 					titleText: res.data.data.title,
-					text: "Account Activated",
+					text: res.data.data.message,
 					icon: "success",
 					allowOutsideClick: false,
 					allowEnterKey: false,
