6c790b2e4ebb016762b2c4a84a994c1fea598cb5
bagus farouk <b46usf@gmail.com>
2024-04-18 19:47:53 +0700

    commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -24,14 +24,14 @@ export default function Login() {
 		apiServices
 			.toLogin(token_key[0], getFormData(keys, values))
 			.then((response) => {
-				console.log(parseJwt(response.data.data.Tokenjwt, "fromToken"));
+				console.log(parseJwt(response.data.data.Tokenjwt));
 				localStorage.setItem("login_token", response.data.data.Tokenjwt);
 				const keys = ["AUTH_KEY", "devop-sso", "csrf_token", "token"];
 				const values = [
 					localStorage.getItem("AUTH_KEY"),
 					localStorage.getItem("devop-sso"),
 					response.data.csrfHash,
-					parseJwt(response.data.data.Tokenjwt, "fromToken"),
+					parseJwt(response.data.data.Tokenjwt),
 					// localStorage.getItem("login_token"),
 				];
 				apiServices
