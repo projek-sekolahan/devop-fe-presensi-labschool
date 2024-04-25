432ce09f38227f6b095d1815a146cc9daedc8f2b
bagus farouk <b46usf@gmail.com>
2024-04-25 08:49:55 +0700

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -45,6 +45,23 @@ export default function Home() {
 				)
 			}
 		}).catch((err) => {
+			localStorage.clear();
+			if(err.status == 403) {
+				alert(
+					"error",
+					"Credential Expired",
+					"Your credentials has expired. Please login again.",
+					() => window.location.replace("/login"),
+				)
+			} else {
+				err = JSON.parse(err.responseText);
+				alert(
+					err.data.info,
+					err.data.title,
+					err.data.message,
+					() => window.location.replace("/login"),
+				)
+			}
 			console.log(err);
 		})
 	};
