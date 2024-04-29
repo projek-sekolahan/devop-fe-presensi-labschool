3e2615c6eb48e2e464814e3cd1bf7427310d28db
Ijoelel <fianarnatha13@gmail.com>
2024-04-29 21:39:38 +0700

    29-04-2024

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -15,8 +15,6 @@ export default function Profile() {
 		localStorage.getItem("csrf"),
 		localStorage.getItem("login_token"),
 	];
-	console.log(keys);
-	console.log(values);
 
 	apiXML
 		.getUserData(
