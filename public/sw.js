f7189f75b4e8e69826da504bcf22ccfb5de48a37
Ijoelel <fianarnatha13@gmail.com>
2024-05-13 21:49:23 +0700

    13-05-2024

--- public/sw.js
+++ public/sw.js
@@ -1,68 +1,7 @@
-const CACHE = "pwabuilder-offline-page";
-
-importScripts(
-	"https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
-);
-
-const offlineFallbackPage = "offline.html";
-
-self.addEventListener("message", (event) => {
-	if (event.data && event.data.type === "SKIP_WAITING") {
-		self.skipWaiting();
-	}
-});
-
-self.addEventListener("install", async (event) => {
-	event.waitUntil(
-		caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
-	);
-});
-
-if (workbox.navigationPreload.isSupported()) {
-	workbox.navigationPreload.enable();
-}
-
-workbox.routing.registerRoute(
-	new RegExp("/*"),
-	new workbox.strategies.StaleWhileRevalidate({
-		cacheName: CACHE,
-	})
-);
-
-self.addEventListener("fetch", (event) => {
-	if (event.request.mode === "navigate") {
-		event.respondWith(
-			(async () => {
-				try {
-					const preloadResp = await event.preloadResponse;
-
-					if (preloadResp) {
-						return preloadResp;
-					}
-
-					const networkResp = await fetch(event.request);
-					return networkResp;
-				} catch (error) {
-					const cache = await caches.open(CACHE);
-					const cachedResp = await cache.match(offlineFallbackPage);
-					return cachedResp;
-				}
-			})()
-		);
-	}
-});
-
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries
-// are not available in the service worker.
 importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts(
 	"https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
 );
-
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
 firebase.initializeApp({
 	apiKey: "AIzaSyANCfphvM408UXtVutV3s3JUWcv50Wox4s",
 	authDomain: "projek-sekolah-1acb4.firebaseapp.com",
