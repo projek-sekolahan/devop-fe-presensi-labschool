f74a7b865cefd20f2b9f82e6fabde97546784cf2
Ijoelel <fianarnatha13@gmail.com>
2024-05-14 00:22:52 +0700

    13-05-2024

--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,56 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+
+const CACHE = "pwabuilder-offline-page";
+
 importScripts(
-	"https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+	"https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
 );
 
-firebase.initializeApp({
-	apiKey: "AIzaSyANCfphvM408UXtVutV3s3JUWcv50Wox4s",
-	authDomain: "projek-sekolah-1acb4.firebaseapp.com",
-	projectId: "projek-sekolah-1acb4",
-	storageBucket: "projek-sekolah-1acb4.appspot.com",
-	messagingSenderId: "796889279454",
-	appId: "1:796889279454:web:b9c53d12f01f3551f38b4f",
-	measurementId: "G-NWG3GGV7DF",
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline.html";
+
+self.addEventListener("message", (event) => {
+	if (event.data && event.data.type === "SKIP_WAITING") {
+		self.skipWaiting();
+	}
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(messaging, (payload) => {
-	console.log(
-		"[firebase-messaging-sw.js] Received background message ",
-		payload
+self.addEventListener("install", async (event) => {
+	event.waitUntil(
+		caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
 	);
-	// Customize notification here
-	const notificationTitle = "Background Message Title";
-	const notificationOptions = {
-		body: "Background Message body.",
-		icon: "/firebase-logo.png",
-	};
+});
 
-	self.registration.showNotification(notificationTitle, notificationOptions);
+if (workbox.navigationPreload.isSupported()) {
+	workbox.navigationPreload.enable();
+}
+
+workbox.routing.registerRoute(
+	new RegExp("/*"),
+	new workbox.strategies.StaleWhileRevalidate({
+		cacheName: CACHE,
+	})
+);
+
+self.addEventListener("fetch", (event) => {
+	if (event.request.mode === "navigate") {
+		event.respondWith(
+			(async () => {
+				try {
+					const preloadResp = await event.preloadResponse;
+
+					if (preloadResp) {
+						return preloadResp;
+					}
+
+					const networkResp = await fetch(event.request);
+					return networkResp;
+				} catch (error) {
+					const cache = await caches.open(CACHE);
+					const cachedResp = await cache.match(offlineFallbackPage);
+					return cachedResp;
+				}
+			})()
+		);
+	}
 });
