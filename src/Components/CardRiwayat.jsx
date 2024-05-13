7cb24f0fb8e0434289a20b2903a042dbe3a2f4e1
Ijoelel <fianarnatha13@gmail.com>
2024-05-14 05:23:09 +0700

    13-05-2024

--- src/Components/CardRiwayat.jsx
+++ src/Components/CardRiwayat.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 export default function CardRiwayat({ key, history, biodata }) {
 	const [datas, setDatas] = useState(null);
-	const [load, setLoad] = useState(true);
+	const [loading, setLoading] = useState(true);
 
 	const clickHandler = () => {
 		document.getElementById(`my_modal_${key}`).showModal();
