75206f08b3362d1d130ecfc31252f9120d6d8b8c
bagus farouk <b46usf@gmail.com>
2024-05-15 20:04:51 +0700

    commit

--- public/sw.js
+++ public/sw.js
@@ -58,31 +58,46 @@ self.addEventListener("fetch", (event) => {
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here. Other Firebase libraries
 // are not available in the service worker.
-importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+/* importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts(
 	"https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
-);
+); */
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-firebase.initializeApp({
-	apiKey: "AIzaSyANCfphvM408UXtVutV3s3JUWcv50Wox4s",
-	authDomain: "projek-sekolah-1acb4.firebaseapp.com",
-	projectId: "projek-sekolah-1acb4",
-	storageBucket: "projek-sekolah-1acb4.appspot.com",
-	messagingSenderId: "796889279454",
-	appId: "1:796889279454:web:b9c53d12f01f3551f38b4f",
-	measurementId: "G-NWG3GGV7DF",
-});
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getMessaging, onMessage } from "firebase/messaging/sw";
+// import { getAnalytics } from "firebase/analytics";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyANCfphvM408UXtVutV3s3JUWcv50Wox4s",
+  authDomain: "projek-sekolah-1acb4.firebaseapp.com",
+  projectId: "projek-sekolah-1acb4",
+  storageBucket: "projek-sekolah-1acb4.appspot.com",
+  messagingSenderId: "796889279454",
+  appId: "1:796889279454:web:b9c53d12f01f3551f38b4f",
+  measurementId: "G-NWG3GGV7DF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app);
+// const analytics = getAnalytics(app);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = firebase.messaging();
-/* messaging.onMessage((payload) => {
-    console.log('Message received. ', payload);
-}); */
-messaging.onBackgroundMessage((payload) => {
+const messaging = getMessaging(firebaseConfig);
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload);
+  // ...
+});
+
+/* messaging.onBackgroundMessage((payload) => {
 	console.log(
 		"[firebase-messaging-sw.js] Received background message ",
 		payload
@@ -95,4 +110,4 @@ messaging.onBackgroundMessage((payload) => {
 	};
 
 	self.registration.showNotification(notificationTitle, notificationOptions);
-});
+}); */
