44785c295bdd5aade2ba78a1758ac5bdf1afd850
bagus farouk <b46usf@gmail.com>
2024-05-15 22:52:48 +0700

    commit

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -26,11 +26,11 @@ messaging.onBackgroundMessage((payload) => {
 		payload
 	);
 	// Customize notification here
-	const notificationTitle = "Background Message Title";
-	const notificationOptions = {
-		body: "Background Message body.",
-		icon: "/firebase-logo.png",
-	};
+	const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        // icon: '/firebase-logo.png'
+    };
 
 	self.registration.showNotification(notificationTitle, notificationOptions);
 });
