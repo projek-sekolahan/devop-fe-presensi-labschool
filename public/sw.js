96607940265c29a1575c70fea3c048e120137bbe
bagus farouk <b46usf@gmail.com>
2024-05-15 20:21:25 +0700

    commit

--- public/sw.js
+++ public/sw.js
@@ -6,7 +6,6 @@ importScripts(
 	"https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
 );
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "offline.html";
 
 self.addEventListener("message", (event) => {
@@ -54,58 +53,3 @@ self.addEventListener("fetch", (event) => {
 		);
 	}
 });
-
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries
-// are not available in the service worker.
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js");
-
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
-// Import the functions you need from the SDKs you need
-/* import { initializeApp } from "firebase/app";
-import { getMessaging, onMessage } from "firebase/messaging/sw"; */
-// import { getAnalytics } from "firebase/analytics";
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyANCfphvM408UXtVutV3s3JUWcv50Wox4s",
-  authDomain: "projek-sekolah-1acb4.firebaseapp.com",
-  projectId: "projek-sekolah-1acb4",
-  storageBucket: "projek-sekolah-1acb4.appspot.com",
-  messagingSenderId: "796889279454",
-  appId: "1:796889279454:web:b9c53d12f01f3551f38b4f",
-  measurementId: "G-NWG3GGV7DF"
-};
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-console.log(app);
-// const analytics = getAnalytics(app);
-
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging();
-messaging.onMessage((payload) => {
-    console.log('Message received. ', payload);
-    // ...
-});
-
-/* messaging.onBackgroundMessage((payload) => {
-	console.log(
-		"[firebase-messaging-sw.js] Received background message ",
-		payload
-	);
-	// Customize notification here
-	const notificationTitle = "Background Message Title";
-	const notificationOptions = {
-		body: "Background Message body.",
-		icon: "/firebase-logo.png",
-	};
-
-	self.registration.showNotification(notificationTitle, notificationOptions);
-}); */
