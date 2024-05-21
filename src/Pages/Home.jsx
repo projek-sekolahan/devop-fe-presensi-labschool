bfd90bca43ca459f1491364d806d1fd92af2e9d1
Ijoelel <fianarnatha13@gmail.com>
2024-05-21 14:06:35 +0700

    21-05-2024

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -56,6 +56,7 @@ const Home = () => {
                 localStorage.setItem("token", res.data);
                 Cookies.set("csrf", res.csrfHash);
                 const user = parseJwt(res.data);
+                console.log(user);
                 setUserData(user);
             } else {
                 console.error("No data in API response:", res);
