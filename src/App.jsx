718df9c4da4a9763c3e4118720a323a5f785b779
Ijoelel <fianarnatha13@gmail.com>
2024-06-08 19:25:17 +0700

    08-06-2024

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 	}, []);
 	return (
 		<Router>
-			{width && height && height >= width ? (
+			{width && height && height <= width ? (
 				<div className="font-primary w-screen h-screen absolute left-0 top-0 z-50 flex justify-center items-center before:size-full before:bg-black before:opacity-40 backdrop-blur-sm before:absolute">
 					<div className="modal-box">
 						<h3 className="font-bold text-xl">Warning!</h3>
