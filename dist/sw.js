dea64fcfab8a13ed04a1934359187db1ec9458e8
Ijoelel <fianarnatha13@gmail.com>
2024-12-14 11:39:28 +0700

    update action

---
+++ dist/sw.js
@@ -0,0 +1,98 @@
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+
+const CACHE = "pwabuilder-offline-page";
+
+importScripts(
+	"https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js",
+);
+
+const offlineFallbackPage = "offline.html";
+
+const options = { tag: "user_alerts" };
+
+self.registration.getNotifications(options);
+
+self.addEventListener("message", (event) => {
+	if (event.data && event.data.type === "SKIP_WAITING") {
+		self.skipWaiting();
+	}
+});
+
+self.addEventListener("install", async (event) => {
+	event.waitUntil(
+		caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage)),
+	);
+});
+
+if (workbox.navigationPreload.isSupported()) {
+	workbox.navigationPreload.enable();
+}
+
+workbox.routing.registerRoute(
+	new RegExp("/*"),
+	new workbox.strategies.StaleWhileRevalidate({
+		cacheName: CACHE,
+	}),
+);
+
+self.addEventListener("fetch", (event) => {
+	if (event.request.mode === "navigate") {
+		event.respondWith(
+			(async () => {
+				try {
+					const preloadResp = await event.preloadResponse;
+
+					if (preloadResp) {
+						return preloadResp;
+					}
+
+					const networkResp = await fetch(event.request);
+					return networkResp;
+				} catch (error) {
+					const cache = await caches.open(CACHE);
+					const cachedResp = await cache.match(offlineFallbackPage);
+					return cachedResp;
+				}
+			})(),
+		);
+	}
+});
+
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts(
+	"https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js",
+);
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+	apiKey: "AIzaSyANCfphvM408UXtVutV3s3JUWcv50Wox4s",
+	authDomain: "projek-sekolah-1acb4.firebaseapp.com",
+	projectId: "projek-sekolah-1acb4",
+	storageBucket: "projek-sekolah-1acb4.appspot.com",
+	messagingSenderId: "796889279454",
+	appId: "1:796889279454:web:b9c53d12f01f3551f38b4f",
+	measurementId: "G-NWG3GGV7DF",
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+	console.log(
+		"[firebase-messaging-sw.js] Received background message ",
+		payload,
+	);
+	// Customize notification here
+	const notificationTitle = payload.notification.title;
+	const notificationOptions = {
+		body: payload.notification.body,
+		// icon: '/firebase-logo.png'
+	};
+
+	self.registration.showNotification(notificationTitle, notificationOptions);
+});
