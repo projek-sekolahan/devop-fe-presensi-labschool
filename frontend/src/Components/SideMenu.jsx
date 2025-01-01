f8221dbd477173f7164b0975a55631eb79a36316
bagus farouk <b46usf@gmail.com>
2025-01-01 20:52:31 +0700

    commit

--- frontend/src/Components/SideMenu.jsx
+++ frontend/src/Components/SideMenu.jsx
@@ -5,7 +5,7 @@ import {
 	ChevronRightIcon,
 	XMarkIcon,
 	PowerIcon,
-	ArrowRightIcon,
+	ArrowRightEndIcon,
 } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 import apiXML from "../utils/apiXML";
@@ -145,7 +145,7 @@ export default function SideMenu({ show, userData, closeMenu }) {
         >
           <PowerIcon className="w-5 h-5 text-white" />
           Logout
-          <ArrowRightIcon className="w-5 h-5 text-white ml-auto" />
+          <ArrowRightEndIcon className="w-5 h-5 text-white ml-auto" />
         </Link>
 					</div>
 				</div>
