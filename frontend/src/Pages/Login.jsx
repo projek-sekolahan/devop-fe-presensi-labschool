c373b7a0f863e5dc9f63880f91f18cd2eba12cf4
bagus farouk <b46usf@gmail.com>
2025-01-02 12:59:48 +0700

    commit

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -119,7 +119,7 @@ export default function Login() {
             />
 
             {/* Login Form */}
-            <div className="login-form-container">
+            <div className="login-form-container bg-white p-6 rounded-lg shadow-md z-[2] relative">
                 <h2 className="text-title">Yuk Login!</h2>
                 <p className="text-subtitle">Solusi Pintar Sekolah Digital</p>
                 <form
