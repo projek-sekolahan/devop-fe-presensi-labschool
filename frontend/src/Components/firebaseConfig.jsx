cf26bd455e4cf2ed195c5435e608c3e3ec58c36e
bagus farouk <b46usf@gmail.com>
2025-01-06 03:00:18 +0700

    commit

--- frontend/src/Components/firebaseConfig.jsx
+++ frontend/src/Components/firebaseConfig.jsx
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -7,3 +7,4 @@ export const firebaseConfig = {
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+export default firebaseConfig;
