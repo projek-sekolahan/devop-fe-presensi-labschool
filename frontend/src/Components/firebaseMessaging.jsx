606b6985f9388329ab311a492515004383a4167b
bagus farouk <b46usf@gmail.com>
2025-01-06 15:30:54 +0700

    commit

--- frontend/src/Components/firebaseMessaging.jsx
+++ frontend/src/Components/firebaseMessaging.jsx
@@ -28,7 +28,7 @@ export const registerServiceWorker = async () => {
     }
 
     try {
-        const registration = await navigator.serviceWorker.register("https://smartapps.smalabschoolunesa1.sch.id/frontend/public/firebase-messaging-sw.js?v="+ new Date().getTime(), {
+        const registration = await navigator.serviceWorker.register("https://smartapps.smalabschoolunesa1.sch.id/firebase-messaging-sw.js?v="+ new Date().getTime(), {
             scope: "/"
         });
         
