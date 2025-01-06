8cc46723be81054584f4deb45571cb31450387e4
bagus farouk <b46usf@gmail.com>
2025-01-06 22:04:37 +0700

    commit

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 try {
-  importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js');
+  importScripts('/firebase-app.js');
   console.log("firebase-app.js berhasil dimuat");
-  importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
+  importScripts('/firebase-messaging.js');
   console.log("firebase-messaging.js berhasil dimuat");
 } catch (error) {
   console.error("Error memuat Firebase SDK:", error);
