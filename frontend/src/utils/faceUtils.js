9f2ae56c8085680940f673fbb43b702115c3bc72
Ijoelel <fianarnatha13@gmail.com>
2025-01-08 07:52:43 +0700

    debug infinite loader

--- frontend/src/utils/faceUtils.js
+++ frontend/src/utils/faceUtils.js
@@ -1,12 +1,15 @@
 import * as faceapi from "face-api.js";
 
 export const loadFaceModels = async () => {
+    console.log("loading model");
     const MODEL_URL = "/models";
     await Promise.all([
         faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
         faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
         faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
-    ]);
+    ]).then(() => {
+        console.log("finished loaded model");
+    });
 };
 
 export const detectSingleFace = async (imgElement) => {
