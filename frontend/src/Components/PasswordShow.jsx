a4291bdd6a9446ffafb0ef6b69919eace7cda71f
bagus farouk <b46usf@gmail.com>
2025-01-09 12:49:56 +0700

    commit

--- frontend/src/Components/PasswordShow.jsx
+++ frontend/src/Components/PasswordShow.jsx
@@ -3,7 +3,7 @@ import { forwardRef } from "react";
 
 const PasswordShow = forwardRef(function PasswordShow(props, passwordRef) {
 	return (
-		<label className="password-toggle flex justify-center items-center border-[1px] border-gray-300 bg-white rounded-lg w-10 h-10">
+		<label className="password-toggle flex justify-center items-center border-[1px] border-gray-300 bg-white rounded-lg w-6 h-12">
             <input
                 type="checkbox"
                 className="hidden"
