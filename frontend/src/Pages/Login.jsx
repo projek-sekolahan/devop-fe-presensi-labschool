93fa7949eab75726067f51f4c9150a03492d3a58
bagus farouk <b46usf@gmail.com>
2025-01-12 00:01:52 +0700

    commit

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -102,7 +102,7 @@ export default function Login() {
     };
 
     return (
-<div className="login-container flex flex-col w-full min-h-screen items-center justify-center bg-primary-low">
+<div className="login-container flex flex-col min-h-screen w-screen sm:w-[400px] sm:ml-[calc(50vw-200px)] relative z-[1]">
     {/* Background Image */}
     <img
         src="/frontend/Icons/splash.svg"
