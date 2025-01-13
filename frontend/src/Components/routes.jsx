333ad291e585005cc5b769a595ee688977235896
bagus farouk <b46usf@gmail.com>
2025-01-13 22:00:22 +0700

    commit

--- frontend/src/Components/routes.jsx
+++ frontend/src/Components/routes.jsx
@@ -1,20 +1,20 @@
 import AuthContainer from "../Pages/AuthContainer";
-// import Home from "../Pages/Home";
-// import Profile from "../Pages/Profile";
-// import Presensi from "../Pages/Presensi";
-// import Riwayat from "../Pages/Riwayat";
-// import Bantuan from "../Pages/Bantuan";
-// import Notification from "../Pages/Notification";
-// import PresensiStaff from "../Pages/PresensiStaff";
-// import FaceVerification from "../Pages/FaceVerification";
-// import Izin from "../Pages/Izin";
-// import Pengaturan from "../Pages/Pengaturan";
-// import FaceCam from "../Pages/FaceCam";
-// import Errors from "../Pages/Error";
-// import ProtectedRoute from "../Components/ProtectedRoute";
+import Home from "../Pages/Home";
+import Profile from "../Pages/Profile";
+import Presensi from "../Pages/Presensi";
+import Riwayat from "../Pages/Riwayat";
+import Bantuan from "../Pages/Bantuan";
+import Notification from "../Pages/Notification";
+import PresensiStaff from "../Pages/PresensiStaff";
+import FaceVerification from "../Pages/FaceVerification";
+import Izin from "../Pages/Izin";
+import Pengaturan from "../Pages/Pengaturan";
+import FaceCam from "../Pages/FaceCam";
+import Errors from "../Pages/Error";
+import ProtectedRoute from "../Components/ProtectedRoute";
 
 // Fungsi untuk mengecek autentikasi
-// const isAuthenticated = !!localStorage.getItem("authToken");
+const isAuthenticated = !!localStorage.getItem("authToken");
 
 export const routes = [
     { path: "/", element: <AuthContainer /> },
@@ -26,7 +26,7 @@ export const routes = [
     { path: "/setpassword", element: <AuthContainer /> },
 
     // Rute yang dilindungi
-    /* { path: "/home", element: <ProtectedRoute component={Home} isAuthenticated={isAuthenticated} /> },
+    { path: "/home", element: <ProtectedRoute component={Home} isAuthenticated={isAuthenticated} /> },
     { path: "/profile", element: <ProtectedRoute component={Profile} isAuthenticated={isAuthenticated} /> },
     { path: "/presensi", element: <ProtectedRoute component={Presensi} isAuthenticated={isAuthenticated} /> },
     { path: "/riwayat", element: <ProtectedRoute component={Riwayat} isAuthenticated={isAuthenticated} /> },
@@ -36,8 +36,8 @@ export const routes = [
     { path: "/presensi/verif", element: <ProtectedRoute component={FaceVerification} isAuthenticated={isAuthenticated} /> },
     { path: "/presensi/izin", element: <ProtectedRoute component={Izin} isAuthenticated={isAuthenticated} /> },
     { path: "/setting", element: <ProtectedRoute component={Pengaturan} isAuthenticated={isAuthenticated} /> },
-    { path: "/facecam", element: <ProtectedRoute component={FaceCam} isAuthenticated={isAuthenticated} /> }, */
+    { path: "/facecam", element: <ProtectedRoute component={FaceCam} isAuthenticated={isAuthenticated} /> },
 
     // Fallback untuk rute tidak dikenal
-    // { path: "*", element: <Errors /> },
+    { path: "*", element: <Errors /> },
 ];
