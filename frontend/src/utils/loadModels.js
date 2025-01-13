eac626cdc4313519959bafc4e92ca8be171e119b
bagus farouk <b46usf@gmail.com>
2025-01-13 23:35:20 +0700

    commit

--- frontend/src/utils/loadModels.js
+++ frontend/src/utils/loadModels.js
@@ -3,24 +3,31 @@ import * as faceapi from "face-api.js";
 let modelsLoaded = false;
 
 export const loadFaceModels = async () => {
-    if (!modelsLoaded) {
-        console.log("loading model");
-        const MODEL_URL = "/frontend/models";
-        try {
-            await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
-            console.log("tinyFaceDetector loaded");
+  if (!modelsLoaded) {
+    console.log("loading model");
+    const MODEL_URL = "/frontend/models";
 
-            await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
-            console.log("faceLandmark68Net loaded");
-
-            await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
-            console.log("faceRecognitionNet loaded");
-
-            console.log("finished loaded model");
-            modelsLoaded = true;
-        } catch (error) {
-            console.error("Error loading models:", error);
-        }
+    // Cek lingkungan browser
+    if (typeof window === "undefined" || typeof document === "undefined") {
+      console.error("Error: This code must be run in a browser environment");
+      return;
     }
-    console.log("model loaded");
+
+    try {
+      await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+      console.log("tinyFaceDetector loaded");
+
+      await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
+      console.log("faceLandmark68Net loaded");
+
+      await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
+      console.log("faceRecognitionNet loaded");
+
+      console.log("finished loaded model");
+      modelsLoaded = true;
+    } catch (error) {
+      console.error("Error loading models:", error);
+    }
+  }
+  console.log("model loaded");
 };
