a884bcbf693e7a2809068c1c232c8e932a77127b
bagus farouk <b46usf@gmail.com>
2025-01-13 14:15:38 +0700

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 // index.js
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { registerServiceWorker, requestNotificationPermission, handleOnMessage } from "./Components/firebaseMessaging.jsx";
