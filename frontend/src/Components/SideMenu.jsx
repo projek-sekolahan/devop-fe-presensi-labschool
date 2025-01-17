271533e5396e132a319810c89573eed2cfb5943f
bagus farouk <b46usf@gmail.com>
2025-01-17 10:39:26 +0700

    commit

--- frontend/src/Components/SideMenu.jsx
+++ frontend/src/Components/SideMenu.jsx
@@ -74,83 +74,43 @@ export default function SideMenu({ show, userData, closeMenu, intervalId }) {
 		>
 			<div className="relative w-3/4 h-full bg-white">
 				<div className="size-full px-4">
-					<img
-						src="/frontend/Icons/Ellipse 190.svg"
-						alt=""
-						className="w-full absolute left-0 top-0"
-					/>
-					<div className="flex absolute top-0 left-0 justify-between w-full">
-						<img
-							src="/frontend/Icons/Group 117.svg"
-							alt=""
-							className="w-1/3"
-						/>
-						<img
-							src="/frontend/Icons/Group 116.svg"
-							alt=""
-							className="w-1/3"
-						/>
-						<img
-							src="/frontend/Icons/Group 115.svg"
-							alt=""
-							className="w-1/3"
-						/>
-					</div>
-					<div id="bio" className="absolute top-9 left-4 flex gap-2">
+					<div id="bio">
 						<img
 							src={userData?.img_location || "/default-profile.png"}
 							alt="photo_profile"
-							className="size-12 rounded-full bg-white"
 						/>
-						<div className="font-normal text-base text-bg-2">
+						<div className="user-info">
 							<h4>Halo!</h4>
 							<p>{userData?.nama_lengkap}</p>
 						</div>
 						<button
 						onClick={closeMenu}
-						className="absolute top-0 right-0 text-xl font-bold text-bg-2"
 						>
-						<XMarkIcon className="w-6 h-6 text-bg-2" />
+						<XMarkIcon className="icon" />
 						</button>
 					</div>
-<div
-  id="menu"
-  className="absolute top-40 w-[65vw] sm:w-[65%] text-primary-high flex flex-col gap-4"
->
-  <Link
-    to="/profile"
-    className="w-full bg-bg-2 p-2 rounded-xl flex gap-3 items-center text-base font-medium"
-  >
-    <UserCircleIcon className="w-6 h-6" />
-    <p className="text-base">Profil</p>
-    <ChevronRightIcon className="w-5 h-5 stroke-2 ml-auto" />
-  </Link>
-  <Link
-    to="/setting"
-    className="w-full bg-bg-2 p-2 rounded-xl flex gap-3 items-center text-base font-medium"
-  >
-    <Cog6ToothIcon className="w-6 h-6" />
-    <p className="text-base">Pengaturan</p>
-    <ChevronRightIcon className="w-5 h-5 stroke-2 ml-auto" />
-  </Link>
-  <Link
-    to="/bantuan"
-    className="w-full bg-bg-2 p-2 rounded-xl flex gap-3 items-center text-base font-medium"
-  >
-    <QuestionMarkCircleIcon className="w-6 h-6" />
-    <p className="text-base">Bantuan</p>
-    <ChevronRightIcon className="w-5 h-5 stroke-2 ml-auto" />
-  </Link>
-  <Link
-    to="#"
-    onClick={clickHandler}
-    className="btn border-none text-white w-full p-2 bg-primary-low text-center font-bold rounded-xl mt-4 flex gap-3 items-center text-base"
-  >
-    <PowerIcon className="w-6 h-6" />
-    <p className="text-base">Logout</p>
-    <ArrowRightIcon className="w-5 h-5 stroke-2 ml-auto" />
-  </Link>
-</div>
+					<div id="menu">
+						<Link to="/profile">
+							<UserCircleIcon className="icon" />
+							<p>Profil</p>
+							<ChevronRightIcon className="w-5 h-5 stroke-2 ml-auto" />
+						</Link>
+						<Link to="/setting">
+							<Cog6ToothIcon className="icon" />
+							<p>Pengaturan</p>
+							<ChevronRightIcon className="w-5 h-5 stroke-2 ml-auto" />
+						</Link>
+						<Link to="/bantuan">
+							<QuestionMarkCircleIcon className="icon" />
+							<p>Bantuan</p>
+							<ChevronRightIcon className="w-5 h-5 stroke-2 ml-auto" />
+						</Link>
+						<Link to="#" onClick={clickHandler} className="btn-submit">
+							<PowerIcon className="icon" />
+							<p>Logout</p>
+							<ArrowRightIcon className="w-5 h-5 stroke-2 ml-auto" />
+						</Link>
+					</div>
 				</div>
 			</div>
 		</div>
