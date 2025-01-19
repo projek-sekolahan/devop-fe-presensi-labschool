48bdffb6ce14c2be2ae14142a3b5dea77896dd1c
bagus farouk <b46usf@gmail.com>
2025-01-19 17:21:36 +0700

    commit

--- frontend/src/Pages/PresensiStaff.jsx
+++ frontend/src/Pages/PresensiStaff.jsx
@@ -16,46 +16,10 @@ export default function PresensiStaff() {
 				<Link to="/home">
 					<ArrowLeftIcon className="w-6 h-6 text-white" />
 				</Link>
-
-				<h2 className="text-[2.125rem] font-bold absolute bottom-5">
-					Presensi
-				</h2>
+				<h1 className="presensi-section-container">Presensi</h1>
 			</header>
 			<main>
-				<div className="bg-white w-full rounded-xl p-4 flex flex-col gap-2">
-					<div className="flex justify-between items-center w-full">
-						<p ref={timeRef} className="text-lg font-semibold"></p>
-						<div className="text-lg font-bold">
-							<small ref={dayRef}></small>
-							<small ref={dateRef}></small>
-						</div>
-					</div>
-					<div className="grid grid-cols-2 gap-2 text-white">
-						<Link
-							to="/presensi"
-							state={["non-dinas"]}
-							className="p-5 bg-secondary-green rounded-md flex flex-col justify-center items-center"
-						>
-							<RaiseHandIcon className="size-20" />
-							<p className="text-center font-semibold">
-								Non-Dinas
-							</p>
-						</Link>
-						<Link
-							to="/presensi/izin"
-							state={{ kode: 1, ket: ["dinas-luar"] }}
-							className="p-5 bg-secondary-green rounded-md flex flex-col justify-center items-center"
-						>
-							<img
-								src="/frontend/Icons/exit-run.svg"
-								className="size-20"
-							/>
-							<p className="text-center font-semibold">
-								Dinas Luar
-							</p>
-						</Link>
-					</div>
-				</div>
+				
 			</main>
 		</div>
 	);
