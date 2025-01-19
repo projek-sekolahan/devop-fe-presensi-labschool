1b22262d1d6bac47891992596dfc2a772dd687d5
bagus farouk <b46usf@gmail.com>
2025-01-19 19:46:11 +0700

    commit

--- frontend/src/Pages/PresensiStaff.jsx
+++ frontend/src/Pages/PresensiStaff.jsx
@@ -1,5 +1,5 @@
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
-import RaiseHandIcon from "pepicons/svg/pop/raise-hand.svg?react";
+import { FaHouseUser, FaBuildingUser  } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { useRef } from "react";
 import { useClock } from "../utils/utils";
@@ -26,6 +26,28 @@ export default function PresensiStaff() {
 							<small ref={dayRef}></small>
 							<small ref={dateRef}></small>
 						</div>
+					</div>
+					<div className="grid grid-cols-2 gap-2">
+						<Link
+							to="/presensi"
+							state={["non-dinas"]}
+							className="presensi-icon-container"
+						>
+							<FaHouseUser className="size-20" />
+							<p className="text-center font-semibold">
+								Non-Dinas
+							</p>
+						</Link>
+						<Link
+							to="/presensi/izin"
+							state={{ kode: 1, ket: ["dinas-luar"] }}
+							className="presensi-icon-container"
+						>
+							<FaBuildingUser className="size-20" />
+							<p className="text-center font-semibold">
+								Dinas Luar
+							</p>
+						</Link>
 					</div>
 				</div>
 			</main>
