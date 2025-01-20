a757e7230cae1ee3df53fa4211fb36ebf808ad41
bagus farouk <b46usf@gmail.com>
2025-01-20 23:03:31 +0700

    commit

--- frontend/src/Pages/FaceCam.jsx
+++ frontend/src/Pages/FaceCam.jsx
@@ -245,12 +245,60 @@ export default function FaceCam() {
         </div>
         <div className="absolute flex flex-col items-center gap-12 bottom-10">
           <div className="text-container">
-            <h4 className="font-bold text-3xl mb-2">Verifikasi</h4>
-            <p className="font-semibold">Wajah anda akan diverifikasi untuk memenuhi syarat presensi</p>
+            <p className="font-semibold">
+            {" "}
+            Tekan Tombol{" "}
+            </p>
           </div>
           <div className="w-full px-6 relative">
-            <button className="btn-submit">Verifikasi</button>
+            <button
+              className="btn-submit"
+              disabled={isLoading}
+              onClick={() => {
+                document.getElementById("my_modal_1").showModal();
+                clickPhoto();
+              }}
+            >
+            {isLoading ? (
+              <div className="flex justify-center items-center gap-2">
+                <span>Loading...</span>
+                <span className="loading loading-spinner"></span>
+              </div>
+              ) : (
+                "Presensi"
+              )}
+            </button>
           </div>
+            <dialog id="my_modal_1" className="modal text-black shadow-lg transition transform z-0">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-lg">Hasil Potret</h3>
+                        <p className="text-semibold mt-2 text-gray-600">Cek Hasil Gambar</p>
+                        <img ref={imgRef} className="w-full rounded-lg shadow-md mt-4" />
+                        <div className="modal-action flex justify-center mt-4 gap-4">
+                            <form method="dialog" className="flex gap-4">
+                                {/* if there is a button in form, it will close the modal */}
+                                <button className="py-2 px-4 bg-gray-300 text-black rounded-lg hover:bg-gray-400">
+                                    Cancel
+                                </button>
+                                <button
+                                    className="py-2 px-6 btn-submit"
+                                    onClick={detectFace}
+                                    disabled={isLoading} // Nonaktifkan tombol jika sedang loading
+                                >
+                                {isLoading ? (
+                                    <div className="flex justify-center items-center gap-2">
+                                        <span>Loading...</span>
+                                        <span className="loading loading-spinner text-black"></span>
+                                    </div>
+                                ) : (
+                                    "Proses"
+                                )}
+                                </button>
+                            </form>
+                        </div>
+                    </div>            
+            </dialog>
+            <small>Pastikan Cahaya Terang</small>
         </div>
       </main>
 		</div>
