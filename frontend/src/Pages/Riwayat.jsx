a279b7938496443c419c5d0c5374152d894fd95b
bagus farouk <b46usf@gmail.com>
2025-02-02 20:36:44 +0700

    commit

--- frontend/src/Pages/Riwayat.jsx
+++ frontend/src/Pages/Riwayat.jsx
@@ -1,19 +1,19 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
 import { useState, useEffect, useCallback } from "react";
-import { ArrowLeftIcon, ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import CardRiwayat from "../Components/CardRiwayat";
 import { parseJwt, getFormData, handleSessionError, addDefaultKeys } from "../utils/utils";
 import apiXML from "../utils/apiXML";
 import Cookies from "js-cookie";
-import { Dropdown } from "flowbite-react";
+import { Tabs } from "flowbite-react";
 
 export default function Riwayat() {
-    const [filter, setFilter] = useState("7 Hari");
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const [historyData, setHistoryData] = useState(null);
+    const [historyData, setHistoryData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-
+    const [categories, setCategories] = useState(["7 Hari", "14 Hari", "30 Hari"]);
+    const [activeCategory, setActiveCategory] = useState("7 Hari");
+    
     const userToken = localStorage.getItem("token");
     const userData = userToken ? parseJwt(userToken) : null;
 
@@ -21,66 +21,38 @@ export default function Riwayat() {
         if (!userToken) window.location.replace("/login");
     }, [userToken]);
 
-    const handleClickOutside = useCallback((e) => {
-        const dropdown = document.getElementById("dropdown");
-        const dropdownContent = document.getElementById("dropdown-content");
-        if (dropdown && dropdownContent && !dropdown.contains(e.target)) {
-            setIsDropdownOpen(false);
-        }
-    }, []);
-
-    useEffect(() => {
-        if (isDropdownOpen) {
-            window.addEventListener("click", handleClickOutside);
-        } else {
-            window.removeEventListener("click", handleClickOutside);
-        }
-        return () => window.removeEventListener("click", handleClickOutside);
-    }, [isDropdownOpen, handleClickOutside]);
-
-    const fetchHistory = useCallback(() => {
+    const fetchHistory = useCallback(async () => {
+        setIsLoading(true);
         const keys = ["AUTH_KEY", "token", "table", "key"];
         const combinedKeys = addDefaultKeys(keys);
+        const values = [
+            localStorage.getItem("AUTH_KEY"),
+            localStorage.getItem("login_token"),
+            "tab-presensi",
+            activeCategory === "7 Hari" ? "7 DAY" : activeCategory === "14 Hari" ? "14 DAY" : "30 DAY"
+        ];
 
-        const values = combinedKeys.map((key) => {
-            let value = localStorage.getItem(key);
-            if (key === "csrf_token" && !value) value = Cookies.get("csrf");
-            if (key === "token") value = localStorage.getItem("login_token");
-            if (key === "table" && !value) value = "tab-presensi";
-            if (key === "key" && !value) {
-                value = filter === "7 Hari" ? "7 DAY" : filter === "14 Hari" ? "14 DAY" : "30 DAY";
+        try {
+            const res = await apiXML.presensiPost("reports", localStorage.getItem("AUTH_KEY"), getFormData(combinedKeys, values));
+            const parsedRes = JSON.parse(res);
+            Cookies.set("csrf", parsedRes.csrfHash);
+            setHistoryData(parseJwt(parsedRes.data.token).result);
+        } catch (err) {
+            const parsedErr = JSON.parse(err.responseText);
+            Cookies.set("csrf", parsedErr.csrfHash);
+            if (parsedErr.status === 500) {
+                setHistoryData([]);
+            } else {
+                handleSessionError(parsedErr, "/login");
             }
-            return value;
-        });
-        
-        if (!historyData && isLoading) {
-            apiXML.presensiPost("reports", localStorage.getItem("AUTH_KEY"), getFormData(combinedKeys, values))
-                .then((res) => {
-                    const parsedRes = JSON.parse(res);
-                    Cookies.set("csrf", parsedRes.csrfHash);
-                    setHistoryData(parseJwt(parsedRes.data.token).data);
-                    setIsLoading(false);
-                })
-                .catch((err) => {
-                    const parsedErr = JSON.parse(err);
-                    if (parsedErr.status === 500) {
-                        setIsLoading(false);
-                        setHistoryData([]);
-                    } else {
-                        handleSessionError(parsedErr, "/login");
-                    }
-                });
+        } finally {
+            setIsLoading(false);
         }
-    }, [historyData, isLoading, filter]);
+    }, [activeCategory]);
 
-    useEffect(fetchHistory, [fetchHistory]);
-
-    const handleFilterChange = (newFilter) => {
-        setFilter(newFilter);
-        setIsDropdownOpen(false);
-        setHistoryData(null);
-        setIsLoading(true);
-    };
+    useEffect(() => {
+        fetchHistory();
+    }, [fetchHistory]);
 
     return (
         <div className="notification-container h-screen flex flex-col overflow-y-auto">
@@ -91,45 +63,45 @@ export default function Riwayat() {
                 <h1 className="notification-section-container">Riwayat</h1>
             </header>
             <main className="w-full min-h-screen relative px-8 pt-10 pb-4 text-black flex flex-col gap-4 overflow-y-auto">
-                <div className="w-full mt-[-1.5rem] relative">
-                    <Dropdown label={filter} className="btn bg-white border-none text-bg-3 btn-sm flex justify-between items-center">
-                        {["7 Hari", "14 Hari", "30 Hari"].map((item) => (
-                            <Dropdown.Item key={item} onClick={() => handleFilterChange(item)}>
-                                {item}
-                            </Dropdown.Item>
-                        ))}
-                    </Dropdown>
-                </div>
-                {isLoading ? (
-                    <div className="mt-8 flex justify-center items-center">
-                        <span className="loading-spinner"></span>
-                    </div>
-                ) : historyData?.length ? (
-                    <AnimatePresence>
-                    <motion.div
-                        initial="hidden"
-                        animate="visible"
-                        variants={{
-                            hidden: { opacity: 0, y: 20 },
-                            visible: { opacity: 1, y: 0, transition: { duration: 0.5, staggerChildren: 0.2 } }
-                        }}
-                        className="flex flex-col gap-4"
-                    >
-                        {historyData.map((history, i) => (
-                        <motion.div
-                            key={i}
-                            variants={{ hidden: { opacity: 0, y: 20 }, visible: { opacity: 1, y: 0 } }}
-                        >
-                                <CardRiwayat index={i} history={history} biodata={userData} />
-                        </motion.div>
-                        ))}
-                    </motion.div>
-                    </AnimatePresence>
-                ) : (
-                    <div className="size-full flex justify-center items-center">
-                        <p className="text-white text-xl">Belum ada riwayat.</p>
-                    </div>
-                )}
+                <Tabs
+                    aria-label="Tabs Riwayat"
+                    onActiveTabChange={(tabIndex) => setActiveCategory(categories[tabIndex])}
+                >
+                    {categories.map((category) => (
+                        <Tabs.Item key={category} title={category}>
+                            {isLoading ? (
+                                <div className="flex justify-center items-center">
+                                    <span className="loading-spinner"></span>
+                                </div>
+                            ) : historyData.length ? (
+                                <AnimatePresence>
+                                    <motion.div
+                                        initial="hidden"
+                                        animate="visible"
+                                        variants={{
+                                            hidden: { opacity: 0, y: 20 },
+                                            visible: { opacity: 1, y: 0, transition: { duration: 0.5, staggerChildren: 0.2 } }
+                                        }}
+                                        className="flex flex-col gap-4"
+                                    >
+                                        {historyData.map((history, i) => (
+                                            <motion.div
+                                                key={i}
+                                                variants={{ hidden: { opacity: 0, y: 20 }, visible: { opacity: 1, y: 0 } }}
+                                            >
+                                                <CardRiwayat index={i} history={history} biodata={userData} />
+                                            </motion.div>
+                                        ))}
+                                    </motion.div>
+                                </AnimatePresence>
+                            ) : (
+                                <div className="size-full flex justify-center items-center">
+                                    <p className="text-white text-xl">Belum ada riwayat.</p>
+                                </div>
+                            )}
+                        </Tabs.Item>
+                    ))}
+                </Tabs>
             </main>
         </div>
     );
