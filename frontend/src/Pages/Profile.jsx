b31ee5fbe2afff040af82ccb251aee7cdfe363d1
bagus farouk <b46usf@gmail.com>
2025-02-04 16:10:50 +0700

    commit

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -10,39 +10,7 @@ export default function Profile({ showProfile, userData, closeProfile }) {
       <div className="w-full h-full">
 				<div className="size-full">
           {/* Header Section */}
-          <header className="pt-8">
-            <button onClick={closeProfile}>
-              <ArrowLeftIcon className="w-6 h-6 text-white" />
-            </button>
-            <div className="text-center">
-              <h1 className="profile-section-container">Profile Anda</h1>
-              <div className="mt-6">
-                <img
-                  src={userData.img_location}
-                  alt="User Profile"
-                  className="w-28 h-28 rounded-full mx-auto object-cover border-4 border-white shadow-md"
-                />
-                <p className="font-semibold text-lg mt-4">{`@${userData.email.split('@')[0]}`}</p>
-              </div>
-            </div>
-          </header>
-
-          {/* User Info Section */}
-          <main style="margin-top:20rem; padding:2rem;">
-            {[
-              { label: "Name", value: userData.nama_lengkap },
-              { label: "Account Email", value: userData.email },
-              { label: "Phone Number", value: userData.phone },
-            ].map((item, index) => (
-              <div
-                key={index}
-                className="profile-info"
-              >
-                <h4>{item.label}</h4>
-                <p>{item.value}</p>
-              </div>
-            ))}
-          </main>
+          
         </div>
       </div>
     </div>
