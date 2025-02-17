06e870f8b2d86b3c692ead7b89b50059ce3ebd21
bagus farouk <b46usf@gmail.com>
2025-02-17 11:13:28 +0700

    commit

--- frontend/src/Pages/ChangePassword.jsx
+++ frontend/src/Pages/ChangePassword.jsx
@@ -32,15 +32,19 @@ export default function ChangePassword({ isOpen, onToggle }) {
         const values = [emailRef.current.value.trim(), Cookies.get("csrf")];
         localStorage.setItem("email", emailRef.current.value);
 
-        try {
-            const res = await ApiService.processApiRequest("recover", getFormData(keys, values), () => onToggle(res.data.location));
+        console.log(typeof getFormData(keys, values));
+        const params = new URLSearchParams(getFormData(keys, values));
+        let AUTH_KEY = params.get("AUTH_KEY");
+        console.log(AUTH_KEY);
+        /* try {
+            const res = await ApiService.processApiRequest("recover", getFormData(keys, values));
             console.log(res); return false;
             Cookies.set("csrf", res.csrfHash);
             setLoad(false);
             alertMessage(res.data.title, res.data.message, res.data.info, () => onToggle(res.data.location));
         } catch (err) {
             handleSessionError(err, "/recover");
-        }
+        } */
     };
 
     return (
