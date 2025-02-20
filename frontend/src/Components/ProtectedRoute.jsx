d397d0f5dbdb3ff171b8b8f250d47c243f99d132
bagus farouk <b46usf@gmail.com>
2025-02-21 06:43:26 +0700

    commit

--- frontend/src/Components/ProtectedRoute.jsx
+++ frontend/src/Components/ProtectedRoute.jsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { checkSession } from "./sessionService"; // Import fungsi checkSession
-import { getFormData, addDefaultKeys } from "../utils/utils";
+import { getFormData, addDefaultKeys, getCombinedValues } from "../utils/utils";
 import Cookies from "js-cookie";
 
 const SESSION_KEYS = ["AUTH_KEY"];
-const getCombinedValues = (keys) => {
+/* const getCombinedValues = (keys) => {
   const combinedKeys = addDefaultKeys(keys);
   return combinedKeys.map((key) => {
     let value = localStorage.getItem(key);
     if (key === "csrf_token" && !value) value = Cookies.get("csrf");
     return value;
   });
-};
+}; */
 
 export default function ProtectedRoute({ component: Component, isAuthenticated }) {
   const [isSessionValid, setIsSessionValid] = useState(true); // Status sesi
@@ -21,9 +21,15 @@ export default function ProtectedRoute({ component: Component, isAuthenticated }
   useEffect(() => {
     const validateSession = async () => {
       if (isAuthenticated) {
+        /* const keys = addDefaultKeys(["param", "AUTH_KEY", "token"]);
+        const formValues = [`${biodata.user_id},${history["Tanggal Presensi"]}`];
+        const storedValues = getCombinedValues(keys.slice(1, 3));
+        const values = [...formValues, ...storedValues];
+        const formData = getFormData(keys, values);
+ */
         const values = getCombinedValues(SESSION_KEYS);
         const formData = getFormData(addDefaultKeys(SESSION_KEYS), values);
-        const isValid = await checkSession(values[0],formData);
+        const isValid = await checkSession(localStorage.getItem("AUTH_KEY"),formData);
         setIsSessionValid(isValid);
       } else {
         setIsSessionValid(false); // Tidak autentikasi berarti sesi invalid
--- frontend/src/Components/sessionService.jsx
+++ frontend/src/Components/sessionService.jsx
@@ -4,7 +4,15 @@ import ApiService from "../utils/ApiService.js";
 import { handleSessionExpired } from "../utils/utils";
 
 export const checkSession = async (keys,formData) => {
-  try {
+  
+  const response = await ApiService.processApiRequest("auth/sesstime", formData, keys, false);
+  console.log("✅ selected Keys:", keys.slice(1, 3));
+  console.log("✅ Final keys:", keys);
+  console.log("✅ Final values:", values);
+  console.log("✅ Final formData:", formData);
+  console.log("✅ Final response:", response?.data);
+
+  /* try {
     const response = await ApiService.authPost("sesstime", keys, formData);
     const parsedResponse = JSON.parse(response);
     if (parsedResponse?.data?.statusCode === 200) {
@@ -21,5 +29,5 @@ export const checkSession = async (keys,formData) => {
       message: error?.message || "Error saat memeriksa sesi",
     });
     return false; // Sesi tidak valid
-  }
+  } */
 };
