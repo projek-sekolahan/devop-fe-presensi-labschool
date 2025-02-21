148b80405668a764daf164d23a020d63c63c4f9f
bagus farouk <b46usf@gmail.com>
2025-02-21 09:03:45 +0700

    commit

--- frontend/src/Components/sessionService.jsx
+++ frontend/src/Components/sessionService.jsx
@@ -2,27 +2,11 @@ import ApiService from "../utils/ApiService.js";
 import { handleSessionExpired } from "../utils/utils";
 
 export const checkSession = async (keys,formData) => {
-  
   const response = await ApiService.processApiRequest("auth/sesstime", formData, keys, false);
-  console.log("✅ Final formData:", formData);
-  console.log("✅ Final response:", response?.data);
-
-  /* try {
-    const response = await ApiService.authPost("sesstime", keys, formData);
-    const parsedResponse = JSON.parse(response);
-    if (parsedResponse?.data?.statusCode === 200) {
-      Cookies.set("csrf", parsedResponse.csrfHash);
-      return true; // Sesi valid
-    } else {
-      handleSessionExpired(parsedResponse?.data || { title: "Session Expired", message: "Sesi Anda telah berakhir." });
-      return false; // Sesi tidak valid
-    }
-  } catch (error) {
-    console.error("Error saat memeriksa sesi:", error);
-    handleSessionExpired({
-      title: "Session Timeout",
-      message: error?.message || "Error saat memeriksa sesi",
-    });
-    return false; // Sesi tidak valid
-  } */
+  if (response?.data?.statusCode === 200) {
+    return true;
+  } else {
+    handleSessionExpired(parsedResponse?.data || { title: "Session Expired", message: "Sesi Anda telah berakhir." });
+    return false;
+  }
 };
