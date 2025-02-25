70277360a1d4950f3cdb53b1e8a06b5c0a189a67
Ijoelel <fianarnatha13@gmail.com>
2025-02-25 09:42:29 +0700

    fix hard to match face

--- frontend/src/utils/useFaceRecognition.js
+++ frontend/src/utils/useFaceRecognition.js
@@ -4,7 +4,8 @@ export const detectFace = async (imgElement) => {
     if (!imgElement || !(imgElement instanceof HTMLImageElement)) {
         throw new Error("Invalid image element.");
     }
-    const detection = await detectSingleFace(imgElement); console.log("face recognis" , detection);
+    const detection = await detectSingleFace(imgElement);
+    console.log("face recognis", detection);
     if (!detection || typeof detection.descriptor === "undefined") {
         throw new Error("Face not detected.");
     }
@@ -12,13 +13,17 @@ export const detectFace = async (imgElement) => {
 };
 
 export const compareFaces = async (detectionResult, tokenDescriptor) => {
-    if (!detectionResult?.descriptor) {
+    console.log("Detection Resul on Compare : ", detectionResult);
+    if (!detectionResult) {
         throw new Error("Invalid detection result.");
     }
-    const isFaceMatched = await validateFaceDetection(detectionResult.descriptor, tokenDescriptor);
+    const isFaceMatched = await validateFaceDetection(
+        detectionResult,
+        tokenDescriptor
+    );
     if (typeof isFaceMatched !== "boolean" || !isFaceMatched) {
         throw new Error("Face match failed.");
     }
-    return detectionResult.descriptor;
+    return detectionResult;
 };
 // Tidak perlu custom hook karena ini murni fungsi utility
